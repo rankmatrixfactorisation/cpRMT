8461ab01622b34e580ab2845db7424d63605d4f5
Thanh Le Van <thanhlv@gmail.com>
2016-11-28 09:30:07 +0100

    add readme.cmd for SUSHI dataset

---
+++ data/sushi/readme.cmd
@@ -0,0 +1,7 @@
+SUSHI dataset [1]
+=====================================================
+
++ tkde_sushi3a.txt: rankings of 10 sushis. 
+
+[1] Kamishima, T. Nantonac collaborative filtering: Recommendation based on order responses. In Proceedings of the Ninth ACM SIGKDD
+International Conference on Knowledge Discovery and Data Mining (2003), KDD ’03, ACM, pp. 583–588.
