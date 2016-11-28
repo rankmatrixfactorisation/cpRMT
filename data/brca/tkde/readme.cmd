0434c393d7ae334e61c630bd76ceed9823fd487d
Thanh Le Van <thanhlv@gmail.com>
2016-11-28 09:26:17 +0100

    update readme for tkde brca

--- data/brca/tkde/readme.cmd
+++ data/brca/tkde/readme.cmd
@@ -1,3 +1,9 @@
 TCGA breast cancer dataset [1]
 =======================================
 
++ tkde_*_subset.txt: selected molecular data type (numeric values)
++ tkde_*_rank.txt: rank version of the corresponding molecular data
++ tkde_mrna_mirna_protein_cnv_rank_data_wo_names.txt: the single rank matrix used in the algorithm. This matrix is a combination of mrna, mirna, protein and cnv data in that order.
+
+[1] Cancer Genome Atlas Network. Comprehensive molecular portraits of human breast tumours. Nature 490, 7418 (Oct. 2012), 61–70.
+
