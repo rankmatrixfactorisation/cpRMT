cd7dbd1c462550d1b5ae911051222c966b9433ad
Thanh Le Van <thanhlv@gmail.com>
2016-11-28 09:19:01 +0100

    update readme for the ESC dataset

--- data/EUSongContest/readme.cmd
+++ data/EUSongContest/readme.cmd
@@ -1,1 +1,6 @@
 European Song Festivals (2010 - 2013)
+--------------------------------------
+
++ num_avg_2010_2013.txt: average voting values during the studying period (numeric data).
++ rank_avg_2010_2013_min_break.txt: rank version of the numeric voting data, using the minimum rank value when there is a tie.
+
